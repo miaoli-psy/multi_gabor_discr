4eb013703f1851577243e464a7ce7b4f2c266e67
miao <chuoli223@yahoo.com>
2023-06-16 23:13:31 +0200

    add multi gabor exp3 yes/no results glmer

--- src/analysis/multi_gabor_mixed_models.R
+++ src/analysis/multi_gabor_mixed_models.R
@@ -57,8 +57,8 @@ data$setsize <- as.integer(data$trials.setsize)
 
 # LMM
  
-# model.full.interaction <- lmer(trials.intensity ~ condition * setsize + (1 + condition | participant),
-#                                data = data)
+model.full.interaction <- lmer(trials.intensity ~ condition * setsize + (1 + condition | participant),
+                               data = data)
 
 model.full <- lmer(trials.intensity ~ condition + setsize + (1 + condition | participant),
                   data = data)
@@ -83,7 +83,7 @@ summary(emms, infer = TRUE)
 tab_model(model.full, p.val = "kr", show.df = TRUE, show.se = FALSE, show.stat = FALSE)
 
 
-# -------------exp3--------------------------------------------------
+# -------------exp3 ori task---------------------------------------------
 
 # read data
 # exp3: prprcssed_mlti_gbr_sc_3.xlsx
@@ -111,11 +111,11 @@ data3$setsize <- as.integer(data3$setsize)
 
 # LMM
 model.full3 <- lmer(intensity ~ s_l + setsize + (1 + s_l | participant),
-                   data = data)
+                   data = data3)
 
 
 model.reduced3 <- lmer(intensity ~ setsize + (1 + s_l | participant),
-                      data = data)
+                      data = data3)
 
 summary(model.full3)
 anova(model.full3, model.reduced3)
@@ -129,3 +129,37 @@ emms3 <- emmeans(
 
 summary(emms3, infer = TRUE)
 
+
+# -----------------exp3 same or not task---------------------------------------
+
+# get data
+data3_excl1 <- data3[data3$ans_same%in% c("w", 'x'),]
+
+# categorized answer
+data3_excl1 <- data3_excl1 %>% 
+  mutate(ans = case_when(
+    ans_same == "w" ~ 1,
+    ans_same == "x" ~ 0)
+         )
+
+colnames(data3_excl1)
+
+data3_excl1$s_l <- as.factor(data3_excl1$s_l)
+data3_excl1$participant <- as.factor(data3_excl1$participant)
+data3_excl1$setsize <- as.integer(data3_excl1$setsize)
+
+# model.glmer.interaction <-glmer(ans ~ setsize * s_l + setsize + s_l + (1 + s_l | participant), data = data3_excl1, family = binomial)
+model.glmer.full <-glmer(ans ~ setsize + s_l + (1 + s_l | participant), data = data3_excl1, family = binomial)
+
+summary(model.glmer.full)
+
+# anova(model.glmer.interaction, model.glmer.full)
+
+# pairwise com
+emms <- emmeans(
+  model.glmer.full,
+  list(pairwise ~ s_l),
+  adjust = "tukey",
+)
+
+summary(emms)
